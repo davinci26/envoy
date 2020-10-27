8323ac2d219516e52f0673a22bec80898a51c68c
Sotiris Nanopoulos <sonanopo@microsoft.com>
2020-10-27 15:02:11 -0700

    format changes
    Signed-off-by: Sotiris Nanopoulos <sonanopo@microsoft.com>

--- source/common/network/connection_impl.cc
+++ source/common/network/connection_impl.cc
@@ -478,7 +478,8 @@ void ConnectionImpl::onWriteBufferLowWatermark() {
   ENVOY_CONN_LOG(debug, "onBelowWriteBufferLowWatermark", *this);
   ASSERT(write_buffer_above_high_watermark_);
   write_buffer_above_high_watermark_ = false;
-  if (Event::optimizeLevelEvents && Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
+  if (Event::optimizeLevelEvents &&
+      Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
     ioHandle().enableFileEvents(ioHandle().getEnabledFileEvents() & ~Event::FileReadyType::Write);
   }
   for (ConnectionCallbacks* callback : callbacks_) {
@@ -490,7 +491,8 @@ void ConnectionImpl::onWriteBufferHighWatermark() {
   ENVOY_CONN_LOG(debug, "onAboveWriteBufferHighWatermark", *this);
   ASSERT(!write_buffer_above_high_watermark_);
   write_buffer_above_high_watermark_ = true;
-  if (Event::optimizeLevelEvents && Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
+  if (Event::optimizeLevelEvents &&
+      Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
     ioHandle().enableFileEvents(ioHandle().getEnabledFileEvents() | Event::FileReadyType::Write);
   }
   for (ConnectionCallbacks* callback : callbacks_) {
@@ -526,7 +528,8 @@ void ConnectionImpl::onFileEvent(uint32_t events) {
   }
 
   if (events & Event::FileReadyType::Write) {
-    if (Event::optimizeLevelEvents && Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
+    if (Event::optimizeLevelEvents &&
+        Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
       ioHandle().enableFileEvents(ioHandle().getEnabledFileEvents() & ~Event::FileReadyType::Write);
     }
     onWriteReady();
@@ -535,7 +538,8 @@ void ConnectionImpl::onFileEvent(uint32_t events) {
   // It's possible for a write event callback to close the socket (which will cause fd_ to be -1).
   // In this case ignore write event processing.
   if (ioHandle().isOpen() && (events & Event::FileReadyType::Read)) {
-    if (Event::optimizeLevelEvents && Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
+    if (Event::optimizeLevelEvents &&
+        Event::PlatformDefaultTriggerType == Event::FileTriggerType::Level) {
       ioHandle().enableFileEvents(ioHandle().getEnabledFileEvents() & ~Event::FileReadyType::Read);
     }
     onReadReady();
